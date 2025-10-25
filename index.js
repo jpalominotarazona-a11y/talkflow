dd4c19f97cf4d3369c3ea17b87e0cc2acd3c4b59
jpalominotarazona-a11y <jpalominotarazona@gmail.com>
2025-10-24 20:37:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'taskflow-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  // añade aquí otros recursos que uses como estilos, scripts
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
